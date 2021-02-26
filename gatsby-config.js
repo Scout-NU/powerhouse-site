d8fdacb267da2f4853cfcd6639d6a87f1550caa4
samgildea <gildea.s@husky.neu.edu>
2021-02-26 13:24:32 -0500

    testing prismic schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,41 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@walltowall/gatsby-source-prismic-schemas',
+      options: {
+        /**
+         * Provide an object of Prismic custom type JSON schemas to load into
+         * Gatsby. This is required.
+         *
+         * If you are using gatsby-source-prismic, this should be the same set of
+         * schemas provided to that plugin.
+         */
+        schemas: {
+          // Your custom types mapped to schemas
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        // The name of your prismic.io repository. This is required.
+        // Example: 'gatsby-source-prismic-test-site' if your prismic.io address
+        // is 'gatsby-source-prismic-test-site.prismic.io'.
+        repositoryName: 'power-house',
+        schemas: {
+          // all the schemas here
+          test: require("./src/schemas/test.json"),
+
+       },
+       typePathsFilenamePrefix:
+       'prismic-typepaths---powerhouse-site',
+      }
+    },
+
+
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Prismic Site Coming!</div>
+export default function Home({ data }) {
+  return <div>{data.prismicTestContent.data.test_title[0].text}</div>
 }
+
+export const query = graphql`
+  query TestQuery {
+    prismicTestContent {
+      data {
+        test_title {
+          text
+          type
+        }
+      }
+    }
+  }
+`
