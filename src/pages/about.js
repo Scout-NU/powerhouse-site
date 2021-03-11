baf0ef02b33eea0f9556bf416787662e62625d20
Viviana <camargo.v@northeastern.edu>
2021-03-11 11:18:33 -0500

    #2 Added group content types to different sections of About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,19 +14,37 @@ export default function About({ data }) {
 
 export const query = graphql`
   query AboutQuery {
-    prismicAbout {
+    prismicAboutPage {
       data {
+        company {
+          company_name
+        }
         company_background_description
         company_background_section_heading
+        company_comparison_section_heading
         company_team_heading
         cta_button_destination
         cta_button_text
+        industry_context_explanation {
+          explanation_description
+        }
         industry_context_heading
         mission_description
         mission_heading
+        product_benefit {
+          product_benefit_title
+        }
+        product_feature {
+          product_feature_name
+        }
         products_navigation_button_destination
         products_navigation_button_text
-        company_comparison_section_heading
+        team_member_information {
+          contact_button_destination
+          contact_button_text
+          team_member_description
+          team_member_title
+        }
       }
     }
   }
