06cafb38da730479b18da679df4630adaa9e3950
samgildea <gildea.s@husky.neu.edu>
2021-03-12 11:52:42 -0500

    #5 create initial layout and style structure

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,57 @@
 import React from "react"
 import styled from "styled-components"
 import "../style/typography.scss"
-import globalStyles from "../style/global"
-
 
 import dimensions from "../style/dimensions"
 import { layoutPaddingDesktop, layoutPaddingMobile } from "../style/variables"
 
 const LayoutContainer = styled.div`
+  body * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  html,
+  body,
+  #root {
+    margin: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    min-height: 100%;
+  }
+
+  body {
+    width: 100%;
+    margin: 0 auto;
+    font-size: 16px;
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+
+    @media (max-width: ${dimensions.maxwidthMobile}px) {
+      font-size: 14px;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+  }
+
+  /*
+  A workaround for forcing accessibility wrappers
+  to have a 100% height.
+  Reach Router issue here: https: //github.com/reach/router/issues/63
+  */
+  #___gatsby,
+  div[role="group"][tabindex] {
+    height: 100%;
+    min-height: 100% !important;
+  }
+
   padding: 0 ${layoutPaddingDesktop} ${layoutPaddingDesktop};
 
   margin: 0 auto;
-
+`
+const LayoutBody = styled.div`
   @media (max-width: ${dimensions.maxwidthTablet}px) {
     padding: 0 ${layoutPaddingDesktop} ${layoutPaddingDesktop};
   }
@@ -20,11 +60,10 @@ const LayoutContainer = styled.div`
     padding: 0 ${layoutPaddingMobile} ${layoutPaddingMobile};
   }
 `
-const LayoutBody = styled.div``
 
 const Layout = ({ children }) => {
   return (
-    <LayoutContainer styles={[globalStyles]} >
+    <LayoutContainer>
       <LayoutBody>{children}</LayoutBody>
     </LayoutContainer>
   )
