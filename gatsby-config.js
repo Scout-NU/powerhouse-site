1b2a9d8495b395f6fd5a2720481ce7a6146ee002
samgildea <gildea.s@husky.neu.edu>
2021-03-24 16:40:37 -0400

    change process title in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
           test: require("./src/schemas/test.json"),
           product_page: require("./src/schemas/product_page.json"),
           product: require("./src/schemas/product.json"),
-          Process: require("./src/schemas/Process.json"),
+          Process: require("./src/schemas/process.json"),
           Homepage: require("./src/schemas/Homepage.json"),
         },
         typePathsFilenamePrefix: "prismic-typepaths---powerhouse-site",
