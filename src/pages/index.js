27c3bebd0b1b568ad9d98b30d7e3c9fbc282cf42
samgildea <gildea.s@husky.neu.edu>
2021-03-26 14:04:50 -0400

    #19 adding lofi hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,70 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import styled from "styled-components"
+import dimensions from "../style/dimensions"
+import { layoutPaddingDesktop, layoutPaddingMobile } from "../style/variables"
+
+const HeroContainer = styled.div`
+  height: 100vh;
+  background-color: #c4c4c4;
+  margin-left: -${layoutPaddingDesktop} !important;
+  width: 100vw;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    margin-left: -${layoutPaddingMobile} !important;
+  }
+`
+
+const HeroTextSection = styled.div`
+  padding-top: 275px;
+  padding-left: ${layoutPaddingDesktop};
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    padding-left: ${layoutPaddingMobile};
+  }
+`
+
+const HeroHeadline = styled.div`
+  font-size: calc(64px + (64 - 32) * ((100vw - 300px) / (1440 - 300)));
+  font-weight: bold;
+`
+
+const HeroDescription = styled.div`
+  font-size: calc(24px + (24 - 16) * ((100vw - 300px) / (1440 - 300)));
+`
+
+const HeroCTA = styled.div`
+  margin-top: 64px;
+  a {
+    background-color: #646464;
+    text-transform: uppercase;
+    color: #ffffff;
+    padding-left: 48px;
+    font-size: 16px;
+    padding-right: 48px;
+    padding-top: 16px;
+    padding-bottom: 16px;
+  }
+`
 
 export default function Home({ data }) {
-
   const homepageData = data.prismicHomepage.data
 
-  return <Layout>{homepageData.hero_headline}</Layout>
+  return (
+    <Layout>
+      <HeroContainer>
+        <HeroTextSection>
+          <HeroHeadline>{homepageData.hero_headline}</HeroHeadline>
+          <HeroDescription>{homepageData.hero_subtext}</HeroDescription>
+          <HeroCTA>
+            <a href={homepageData.hero_button_destination.text}>
+              {homepageData.hero_cta_text}
+            </a>
+          </HeroCTA>
+        </HeroTextSection>
+      </HeroContainer>
+    </Layout>
+  )
 }
 
 export const query = graphql`
