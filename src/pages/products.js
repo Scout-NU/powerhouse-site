2eb2fb50a9c488f663f0f2f74838b4d5e509e44b
Viviana <camargo.v@northeastern.edu>
2021-03-31 20:43:27 -0400

    31 Added styling for Models Section for the Product page. Styling for each individual product requires revision once text for each product is visible.

--- src/pages/products.js
+++ src/pages/products.js
@@ -61,7 +61,63 @@ const FeaturedProductDescription = styled.div`
 
   @media (max-width: ${dimensions.maxwidthTablet}px) {
     padding-top: 15px;
-    
+  }
+`
+
+const ModelScope = styled.div`
+  margin-top: 200px;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    margin-top: 105px;
+  }
+`
+
+const ModelScopeHeading = styled.div`
+  font-size: calc(20px + (20 - 16) * ((100vw - 300px) / (1440 - 300)));
+  text-transform: uppercase;
+`
+
+const ProductsGroup = styled.div`
+  @media (min-width: ${dimensions.maxwidthTablet}px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+const Product = styled.div`
+  padding-top: 33px;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    padding-top: 35px;
+  }
+`
+
+const ProductImage = styled.div`
+  background-color: #C4C4C4;
+  width: 21vw;
+  height: 245px;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    width: 100%;
+  }
+`
+
+const ProductName = styled.div`
+  font-size: calc(30px + (30 - 24) * ((100vw - 300px) / (1440 - 300)));
+  font-weight: bold;
+  padding-top: 49px;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    padding-top: 24px;
+  }
+`
+
+const ProductDescription = styled.div`
+  font-size: 15px;
+  padding-top: 15px;
+
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    visibility: hidden;
   }
 `
 
@@ -79,6 +135,27 @@ export default function ProductSection({ data }) {
           <FeaturedProductDescription>{productSectionData.featured_product_description}</FeaturedProductDescription>
         </FeaturedProductText>
       </FeaturedProductSection>
+
+      <div>
+        {productSectionData.model_scope.map(scope => {
+          return (
+            <ModelScope>
+              <ModelScopeHeading>{scope.model_scope_heading}</ModelScopeHeading>
+              <ProductsGroup>
+                {productSectionData.products.map(product => {
+                  return (
+                    <Product>
+                      <ProductImage></ProductImage>
+                      <ProductName>{product.product_title}</ProductName>
+                      <ProductDescription>{product.product_description}</ProductDescription>
+                    </Product>
+                  )
+                })}
+              </ProductsGroup>
+            </ModelScope>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
