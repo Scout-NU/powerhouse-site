f858fa2824b7472f318e572bf2d2e801f56dfddd
Viviana <camargo.v@northeastern.edu>
2021-04-02 13:19:48 -0400

    #34 Restructured styling components for the singular product page.

---
+++ src/pages/singular-product.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SingularProduct from "../components/singular-product/singular-product"
+
+export default function Home({ data }) {
+  const productData = data.prismicHomepage.data
+
+  return (
+    <Layout>
+      <SingularProduct data={productData} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ProductQuery {
+    prismicProduct {
+        data {
+          button_destination
+          button_title
+          cta_title
+          feature_title
+          features {
+            feature_description {
+              text
+            }
+            feature_image {
+              dimensions {
+                height
+                width
+              }
+              url
+            }
+            feature_name
+          }
+          icon_section_title
+          icons {
+            icon_image {
+              dimensions {
+                height
+                width
+              }
+              url
+            }
+            icon_text
+          }
+          product_description {
+            text
+          }
+          product_images {
+            image {
+              dimensions {
+                height
+                width
+              }
+              url
+            }
+          }
+          product_title
+          product_type
+        }
+      }
+  }
+`
